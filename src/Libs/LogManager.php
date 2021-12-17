3a55da85ff70d4fc11b3f23ab05a423be977c526
Nguyễn Văn Hiệp <nguyenhiepvan.bka@gmail.com>
2021-12-17 14:10:49 +0700

    update log manager

--- src/Libs/LogManager.php
+++ src/Libs/LogManager.php
@@ -10,12 +10,16 @@
 namespace Nguyenhiep\LogShorter\Libs;
 
 
+use Illuminate\Log\Logger;
+
 class LogManager extends \Illuminate\Log\LogManager
 {
+
     /**
      * Attempt to get the log from the local cache.
      *
      * @param string $name
+     * @param array|null $config
      * @return \Psr\Log\LoggerInterface
      */
     protected function get($name, ?array $config = null)
@@ -24,7 +28,7 @@ class LogManager extends \Illuminate\Log\LogManager
             return $this->channels[$name] ?? with($this->resolve($name, $config), function ($logger) use ($name) {
                     return $this->channels[$name] = $this->tap($name, new Logger($logger, $this->app['events']));
                 });
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             return tap($this->createEmergencyLogger(), function ($logger) use ($e) {
                 $logger->emergency('Unable to create configured logger. Using emergency logger.', [
                     'exception' => $e,
